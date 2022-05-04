ac3913ab0a137a5a916653e9eb2f4956e4d1a253
oliver-rdgz <olivert199@hotmail.com>
2022-05-04 16:55:52 -0500

    vinculación finalizado

--- 2 Estructura de Programa/1 Vinculaciones/index.js
+++ 2 Estructura de Programa/1 Vinculaciones/index.js
@@ -1,1 +1,35 @@
-console.log('Vinculaciones')
+console.log('Vinculaciones');
+
+//las variables se representa con la palabra "let"
+let atrapado = 5 * 5;
+
+console.log(atrapado)
+
+console.log(atrapado + 10);
+
+let diez = 10
+
+//es una variable que contiene valor actual de 10, una variable despues de ser vinculada a algun valor también es una expresión que puede ser utilizada para realizar operaciones
+console.log(diez * diez);
+console.log(diez + diez);
+
+//Las variable son datos que pueden cambiar su valor durante la ejecución del programa
+
+let humor = 'ligero';
+
+console.log(humor);
+
+humor = 'oscuro';
+
+console.log(humor);
+
+//Un let puede realizar multiples vinculaciones
+let uno = 1, dos = 2;
+console.log(uno + dos);
+//  resultado: 3
+
+// nombre de las variables: el nombre no debe comenzar con un dígito. El nombre de una vinculación puede incluir signos de dólar ($) o caracteres de subrayado (_), pero no otros signos de puntuación o caracteres especiales.
+
+let $signo1 = "Probando los signos y los números en las variables";
+
+console.log($signo1);
