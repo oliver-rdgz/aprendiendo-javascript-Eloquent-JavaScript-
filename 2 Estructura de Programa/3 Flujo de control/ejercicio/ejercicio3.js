2459926a61b1851fee8e9d3aab516372d2518654
oliver-rdgz <olivert199@hotmail.com>
2022-05-16 15:09:10 -0500

    flujo de control ejercicios finalizados

--- 2 Estructura de Programa/3 Flujo de control/ejercicio/ejercicio3.js
+++ 2 Estructura de Programa/3 Flujo de control/ejercicio/ejercicio3.js
@@ -12,3 +12,26 @@ Escribe un programa que cree un string que represente una cuadrícula de 8 × 8,
  # # # #
 # # # #
 */
+
+
+const tamano = Number(prompt('Eliga el tamaño del tabalo de ajedrez'));
+
+if(isNaN(tamano))
+{
+    console.log('El dato no es un número')
+}else {
+    const tamanoDelTablero = tamano * tamano;
+    let gatitoOEspacio = " ";
+    let tablero = ""
+    for(let index = 1 ; index <= tamanoDelTablero; ++index){
+        tablero += gatitoOEspacio; // 1e 2# 3e 4# 5e
+        gatitoOEspacio = gatitoOEspacio === "#" ? " ": "#";
+        if(index % tamano === 0 && tamano % 2 === 0){
+            tablero += '\n';
+            gatitoOEspacio = gatitoOEspacio === "#" ? " ": "#";
+        }else if (index % tamano === 0 ) {
+            tablero += '\n';
+        }
+    }
+    console.log(tablero);
+};
