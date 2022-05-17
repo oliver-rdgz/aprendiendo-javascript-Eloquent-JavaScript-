86eaf07cf795b64999a411971668e7f36ed893fd
oliver-rdgz <olivert199@hotmail.com>
2022-05-17 17:22:00 -0500

    comenzar funciones en javacript

---
+++ 3 Funciones/1Funciones.js
@@ -0,0 +1,25 @@
+/*
+Una función es un bloque de código que realiza alguna operación. Una función puede definir opcionalmente parámetros de entrada que permiten a los llamadores pasar argumentos a la función. Una función también puede devolver un valor como salida. 
+
+nos permiten dividir el trabajo que hace un programa, en tareas más pequeñas separadas de la parte principal. Ese es el concepto de función en programación.
+
+Bibliografia:
+https://docs.microsoft.com/es-es/cpp/cpp/functions-cpp?view=msvc-170
+https://lenguajesdeprogramacion.net/diccionario/que-es-una-funcion-o-metodo-en-programacion/
+*/
+
+//Construir una función en javascript
+function primeraFuncion(){
+    console.log('Probando primera función');
+}
+//Ejecutar la función en javascript
+primeraFuncion();
+
+//Construir funcion flecha 
+const segundaFuncion = () => {
+    console.log('Probando por segunda vez una función');
+}
+//Ejecutar la función flecha se ejecuta igual que una función en javascript
+segundaFuncion();
+
+
