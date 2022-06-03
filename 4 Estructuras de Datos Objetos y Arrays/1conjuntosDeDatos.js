682015d0828934d3128f81e445f2ef833b2d39e5
oliver-rdgz <olivert199@hotmail.com>
2022-06-03 17:20:47 -0500

    Acceder el valor de un array

---
+++ 4 Estructuras de Datos Objetos y Arrays/1conjuntosDeDatos.js
@@ -0,0 +1,19 @@
+/*
+queremos representar una colección de los números 2, 3, 5, 7 y 11.
+Afortunadamente, JavaScript proporciona un tipo de datos específicamente para almacenar secuencias de valores. Es llamado array y está escrito como una lista de valores entre corchetes, separados por comas.
+*/
+
+let listaDeNumeros = [2, 3, 5, 7, 11];
+
+console.log(listaDeNumeros[2]); 
+// → 5 
+console.log(listaDeNumeros[0]); 
+// → 2 
+console.log(listaDeNumeros[2- 1]); 
+// → 3
+
+// El primer índice de un array es cero, no uno. Entonces el primer elemento es alcanzado con listaDeNumeros[0]
+//listaDeNumeros[ 0 <- indice]
+//Piensa en el índice como la cantidad de elementos a saltar, contando desde el comienzo del array.
+
+//Acceder al valor de un array
