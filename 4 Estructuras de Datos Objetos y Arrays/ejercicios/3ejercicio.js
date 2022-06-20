a9b38e588bdbad56bb66ed726bbc491b2bdb2621
Programador cero <pr0gr4m4d0rc3r0@gmail.com>
2022-06-20 17:22:58 -0500

    ejercicio 3 y 4  de estructuras de datos en proceso

---
+++ 4 Estructuras de Datos Objetos y Arrays/ejercicios/3ejercicio.js
@@ -0,0 +1,18 @@
+/*
+Escribe una función arrayALista que construya una estructura de lista como el que se muestra arriba cuando se le da [1, 2, 3] como argumento. También escribe una función listaAArray que produzca un array de una lista. Luego agrega una función de utilidad preceder, que tome un elemento y una lista y creé una nueva lista que agrega el elemento al frente de la lista de entrada, y posicion, que toma una lista y un número y retorne el elemento en la posición dada en la lista (con cero refiriéndose al primer elemento) o undefined cuando no exista tal elemento. Si aún no lo has hecho, también escribe una versión recursiva de posicion.
+
+let lista = { 
+    valor: 1, 
+    resto: { 
+        valor: 2,
+         resto: { 
+             valor: 3, 
+             resto: null 
+            } 
+    } 
+};
+*/
+
+function arrayALista(array = []){
+    
+}
