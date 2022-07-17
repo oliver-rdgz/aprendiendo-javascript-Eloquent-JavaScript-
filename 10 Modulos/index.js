8943959ee53e5c7093335ae8cda33ac3a2332350
Programador cero <pr0gr4m4d0rc3r0@gmail.com>
2022-07-17 15:29:11 -0500

    estamos en  el layout

--- 10 Modulos/index.js
+++ 10 Modulos/index.js
@@ -2,3 +2,17 @@
 Los módulos son un intento de evitar estos problemas. Un módulo es una pieza del programa que especifica en qué otras piezas este depende ( sus dependencias) y qué funcionalidad proporciona para que otros módulos usen (su interfaz).*/
 
 //Evaluando datos como código
+
+function elt(tipo, ...hijos) { 
+    let nodo = document.createElement(tipo); 
+    for (let hijo of hijos) { 
+        if (typeof hijo != "string") nodo.appendChild(hijo); 
+        else nodo.appendChild(document.createTextNode(hijo)); 
+    } 
+    return nodo; 
+} 
+document.getElementById("cita").appendChild( 
+    elt("footer", "—", 
+    elt("strong", "Karl Popper"), ", prefacio de la segunda edición de ", 
+    elt("em", "La sociedad abierta y sus enemigos"), ", 1950")
+);
